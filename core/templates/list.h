cc8dbc46dda16d5d575ad96e3db284ce51084645
rPatsher <rpatsher@gmail.com>
2024-01-22 15:47:51 +0100

    modified

--- core/templates/list.h
+++ core/templates/list.h
@@ -1,553 +1,146 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <cstddef>
 
-
-
-#define INLINE inline
-template <class T, class A>
-class List { 
+template <typename T>
+class List {
 private:
-    struct Data;
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+    std::size_t size;
 
 public:
-	class Element {
-	private:
-		friend class List<T, A>;
-
-		T value;
-		Element *next_ptr = nullptr;
-		Element *prev_ptr = nullptr;
-        Data data = nullptr;
-
-
-    public:
-		INLINE const Element *next() const {
-			return next_ptr;
-		}
-		INLINE Element *next() {
-			return next_ptr;
-		}
-        INLINE const Element *prev() const {
-			return prev_ptr;
-		}
-        INLINE const T &operator*() const {
-			return value;
-		}
-        INLINE const T *operator->() const {
-			return &value;
-		}
-
-       
-		INLINE T &get() {
-			return value;
-		}
-		/
-		INLINE const T &get() const {
-			return value;
-		}
-		
-		INLINE void set(const T &p_value) {
-			value = (T &)p_value;
-		}
-
-
-        void erase() {
-			data->erase(this);
-		}
-
-		void transfer_to_back(List<T, A> *p_dst_list);
-        INLINE Element() {}
-
-
-    };
-    typedef T ValueType;
-    struct Iterator {
-		INLINE T &operator*() const {
-			return E->get();
-		}
-		INLINE T *operator->() const { return &E->get(); }
-		INLINE Iterator &operator++() {
-			E = E->next();
-			return *this;
-		}
-		INLINE Iterator &operator--() {
-			E = E->prev();
-			return *this;
-		}
-
-		INLINE bool operator==(const Iterator &b) const { return E == b.E; }
-		INLINE bool operator!=(const Iterator &b) const { return E != b.E; }
-
-		Iterator(Element *p_E) { E = p_E; }
-		Iterator() {}
-		Iterator(const Iterator &p_it) { E = p_it.E; }
-
-	private:
-		Element *E = nullptr;
-	};
-
-	struct ConstIterator {
-		INLINE const T &operator*() const {
-			return E->get();
-		}
-		INLINE const T *operator->() const { return &E->get(); }
-		INLINE ConstIterator &operator++() {
-			E = E->next();
-			return *this;
-		}
-		INLINE ConstIterator &operator--() {
-			E = E->prev();
-			return *this;
-		}
-
-		INLINE bool operator==(const ConstIterator &b) const { return E == b.E; }
-		INLINE bool operator!=(const ConstIterator &b) const { return E != b.E; }
-
-		INLINE ConstIterator(const Element *p_E) { E = p_E; }
-		INLINE ConstIterator() {}
-		INLINE ConstIterator(const ConstIterator &p_it) { E = p_it.E; }
-
-	private:
-		const Element *E = nullptr;
-	};
-
-	INLINE Iterator begin() {
-		return Iterator(front());
-	}
-	INLINE Iterator end() {
-		return Iterator(nullptr);
-	}
-
-#if 0
-	//to use when replacing find()
-	INLINE Iterator find(const K &p_key) {
-		return Iterator(find(p_key));
-	}
-#endif
-	INLINE ConstIterator begin() const {
-		return ConstIterator(front());
-	}
-	INLINE ConstIterator end() const {
-		return ConstIterator(nullptr);
-	}
-#if 0
-	//to use when replacing find()
-	INLINE ConstIterator find(const K &p_key) const {
-		return ConstIterator(find(p_key));
-	}
-#endif
-private:
-	struct _Data {
-		Element *first = nullptr;
-		Element *last = nullptr;
-		int size_cache = 0;
-    };
-    _Data *_data = nullptr;
-    public:
-	/**
-	 * return a const iterator to the beginning of the list.
-	 */
-	INLINE const Element *front() const {
-		return _data ? _data->first : nullptr;
-	}
-
-	/**
-	 * return an iterator to the beginning of the list.
-	 */
-	INLINE Element *front() {
-		return _data ? _data->first : nullptr;
-	}
-
-	/**
-	 * return a const iterator to the last member of the list.
-	 */
-	INLINE const Element *back() const {
-		return _data ? _data->last : nullptr;
-	}
-
-	/**
-	 * return an iterator to the last member of the list.
-	 */
-	INLINE Element *back() {
-		return _data ? _data->last : nullptr;
-	}
-
-	/**
-	 * store a new element at the end of the list
-	 */
-	Element *push_back(const T &value) {
-		if (!_data) {
-			_data = memnew_allocator(_Data, A);
-			_data->first = nullptr;
-			_data->last = nullptr;
-			_data->size_cache = 0;
-		}
-
-		Element *n = memnew_allocator(Element, A);
-		n->value = (T &)value;
-
-		n->prev_ptr = _data->last;
-		n->next_ptr = nullptr;
-		n->data = _data;
-
-		if (_data->last) {
-			_data->last->next_ptr = n;
-		}
-
-		_data->last = n;
-
-		if (!_data->first) {
-			_data->first = n;
-		}
-
-		_data->size_cache++;
-
-		return n;
-	}
-
-    void pop_back() {
-		if (_data && _data->last) {
-			erase(_data->last);
-		}
-	}
-
-	/**
-	 * store a new element at the beginning of the list
-	 */
-	Element *push_front(const T &value) {
-		if (!_data) {
-			_data = memnew_allocator(_Data, A);
-			_data->first = nullptr;
-			_data->last = nullptr;
-			_data->size_cache = 0;
-		}
-
-		Element *n = memnew_allocator(Element, A);
-		n->value = (T &)value;
-		n->prev_ptr = nullptr;
-		n->next_ptr = _data->first;
-		n->data = _data;
-
-		if (_data->first) {
-			_data->first->prev_ptr = n;
-		}
-
-		_data->first = n;
-
-		if (!_data->last) {
-			_data->last = n;
-		}
-
-		_data->size_cache++;
-
-		return n;
-	}
-
-	void pop_front() {
-		if (_data && _data->first) {
-			erase(_data->first);
-		}
-	}
-
-	Element *insert_after(Element *p_element, const T &p_value) {
-
-		if (!p_element) {
-			return push_back(p_value);
-		}
-
-		Element *n = memnew_allocator(Element, A);
-		n->value = (T &)p_value;
-		n->prev_ptr = p_element;
-		n->next_ptr = p_element->next_ptr;
-		n->data = _data;
-
-		if (!p_element->next_ptr) {
-			_data->last = n;
-		} else {
-			p_element->next_ptr->prev_ptr = n;
-		}
-
-		p_element->next_ptr = n;
-
-		_data->size_cache++;
-
-		return n;
-	}
-
-	Element *insert_before(Element *p_element, const T &p_value) {
-		if (!p_element) {
-			return push_back(p_value);
-		}
-
-		Element *n = memnew_allocator(Element, A);
-		n->value = (T &)p_value;
-		n->prev_ptr = p_element->prev_ptr;
-		n->next_ptr = p_element;
-		n->data = _data;
-
-		if (!p_element->prev_ptr) {
-			_data->first = n;
-		} else {
-			p_element->prev_ptr->next_ptr = n;
-		}
-
-		p_element->prev_ptr = n;
-
-		_data->size_cache++;
-
-		return n;
-	}
-
-	/**
-	 * find an element in the list,
-	 */
-	template <class T_v>
-	Element *find(const T_v &p_val) {
-		Element *it = front();
-		while (it) {
-			if (it->value == p_val) {
-				return it;
-			}
-			it = it->next();
-		}
-
-		return nullptr;
-	}
-
-	/**
-	 * erase an element in the list, by iterator pointing to it. Return true if it was found/erased.
-	 */
-	bool erase(const Element *p_I) {
-		if (_data && p_I) {
-			bool ret = _data->erase(p_I);
-
-			if (_data->size_cache == 0) {
-				_data = nullptr;
-			}
-
-			return ret;
-		}
-
-		return false;
-	}
-
-	/**
-	 * erase the first element in the list, that contains value
-	 */
-	bool erase(const T &value) {
-		Element *I = find(value);
-		return erase(I);
-	}
-
-	/**
-	 * return whether the list is empty
-	 */
-	INLINE bool is_empty() const {
-		return (!_data || !_data->size_cache);
-	}
-
-	/**
-	 * clear the list
-	 */
-	void clear() {
-		while (front()) {
-			erase(front());
-		}
-	}
-
-	INLINE int size() const {
-		return _data ? _data->size_cache : 0;
-	}
-
-	void swap(Element *p_A, Element *p_B) {
-
-		if (p_A == p_B) {
-			return;
-		}
-		Element *A_prev = p_A->prev_ptr;
-		Element *A_next = p_A->next_ptr;
-		Element *B_prev = p_B->prev_ptr;
-		Element *B_next = p_B->next_ptr;
-
-		if (A_prev) {
-			A_prev->next_ptr = p_B;
-		} else {
-			_data->first = p_B;
-		}
-		if (B_prev) {
-			B_prev->next_ptr = p_A;
-		} else {
-			_data->first = p_A;
-		}
-		if (A_next) {
-			A_next->prev_ptr = p_B;
-		} else {
-			_data->last = p_B;
-		}
-		if (B_next) {
-			B_next->prev_ptr = p_A;
-		} else {
-			_data->last = p_A;
-		}
-		p_A->prev_ptr = A_next == p_B ? p_B : B_prev;
-		p_A->next_ptr = B_next == p_A ? p_B : B_next;
-		p_B->prev_ptr = B_next == p_A ? p_A : A_prev;
-		p_B->next_ptr = A_next == p_B ? p_A : A_next;
-	}
-	/**
-	 * copy the list
-	 */
-	void operator=(const List &p_list) {
-		clear();
-		const Element *it = p_list.front();
-		while (it) {
-			push_back(it->get());
-			it = it->next();
-		}
-	}
-
-	T &operator[](int p_index) {
-		
-		Element *I = front();
-		int c = 0;
-		while (c < p_index) {
-			I = I->next();
-			c++;
-		}
-
-		return I->get();
-	}
-
-	const T &operator[](int p_index) const {
-		
-
-		const Element *I = front();
-		int c = 0;
-		while (c < p_index) {
-			I = I->next();
-			c++;
-		}
-
-		return I->get();
-	}
-
-	void move_to_back(Element *p_I) {
-		
-		if (!p_I->next_ptr) {
-			return;
-		}
-
-		if (_data->first == p_I) {
-			_data->first = p_I->next_ptr;
-		}
-
-		if (_data->last == p_I) {
-			_data->last = p_I->prev_ptr;
-		}
-
-		if (p_I->prev_ptr) {
-			p_I->prev_ptr->next_ptr = p_I->next_ptr;
-		}
-
-		p_I->next_ptr->prev_ptr = p_I->prev_ptr;
-
-		_data->last->next_ptr = p_I;
-		p_I->prev_ptr = _data->last;
-		p_I->next_ptr = nullptr;
-		_data->last = p_I;
-	}
-
-	void reverse() {
-		int s = size() / 2;
-		Element *F = front();
-		Element *B = back();
-		for (int i = 0; i < s; i++) {
-			F = F->next();
-			B = B->prev();
-		}
-	}
-
-	void move_to_front(Element *p_I) {
-		
-		if (!p_I->prev_ptr) {
-			return;
-		}
-
-		if (_data->first == p_I) {
-			_data->first = p_I->next_ptr;
-		}
-
-		if (_data->last == p_I) {
-			_data->last = p_I->prev_ptr;
-		}
-
-		p_I->prev_ptr->next_ptr = p_I->next_ptr;
-
-		if (p_I->next_ptr) {
-			p_I->next_ptr->prev_ptr = p_I->prev_ptr;
-		}
-
-		_data->first->prev_ptr = p_I;
-		p_I->next_ptr = _data->first;
-		p_I->prev_ptr = nullptr;
-		_data->first = p_I;
-	}
-
-	void move_before(Element *value, Element *where) {
-		if (value->prev_ptr) {
-			value->prev_ptr->next_ptr = value->next_ptr;
-		} else {
-			_data->first = value->next_ptr;
-		}
-		if (value->next_ptr) {
-			value->next_ptr->prev_ptr = value->prev_ptr;
-		} else {
-			_data->last = value->prev_ptr;
-		}
-
-		value->next_ptr = where;
-		if (!where) {
-			value->prev_ptr = _data->last;
-			_data->last = value;
-			return;
-		}
-
-		value->prev_ptr = where->prev_ptr;
-
-		if (where->prev_ptr) {
-			where->prev_ptr->next_ptr = value;
-		} else {
-			_data->first = value;
-		}
-
-		where->prev_ptr = value;
-	}
-    List(const List &p_list) {
-		const Element *it = p_list.front();
-		while (it) {
-			push_back(it->get());
-			it = it->next();
-		}
-	}
-
-	List() {}
-
-	~List() {
-		clear();
-		if (_data) {
-			memdelete_allocator<_Data, A>(_data);
-		}
-	}
-
-
-
-
-
+    List() : head(nullptr), size(0) {}
+    ~List();
+
+    void push_back(const T& value);
+    void popBack();
+    void insert(std::size_t index, const T& value);
+    void erase(std::size_t index);
+    T get(std::size_t index) const;
+    void set(std::size_t index, const T& value);
+    std::size_t getSize() const;
+    // Add other methods as needed
 };
 
+template <typename T>
+List<T>::~List() {
+    while (head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
 
+template <typename T>
+void List<T>::push_back(const T& value) {
+    Node* newNode = new Node(value);
+    if (!head) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    size++;
+}
 
+template <typename T>
+void List<T>::popBack() {
+    if (!head) {
+        return;
+    }
+    if (!head->next) {
+        delete head;
+        head = nullptr;
+    } else {
+        Node* current = head;
+        while (current->next && current->next->next) {
+            current = current->next;
+        }
+        delete current->next;
+        current->next = nullptr;
+    }
+    size--;
+}
 
+template <typename T>
+void List<T>::insert(std::size_t index, const T& value) {
+    if (index > size) {
+        return;  // Invalid index
+    }
+    Node* newNode = new Node(value);
+    if (index == 0) {
+        newNode->next = head;
+        head = newNode;
+    } else {
+        Node* current = head;
+        for (std::size_t i = 1; i < index; ++i) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+    size++;
+}
 
-#endif
+template <typename T>
+void List<T>::erase(std::size_t index) {
+    if (index >= size || !head) {
+        return;  // Invalid index or empty list
+    }
+    Node* temp;
+    if (index == 0) {
+        temp = head;
+        head = head->next;
+    } else {
+        Node* current = head;
+        for (std::size_t i = 1; i < index; ++i) {
+            current = current->next;
+        }
+        temp = current->next;
+        current->next = current->next->next;
+    }
+    delete temp;
+    size--;
+}
+
+template <typename T>
+T List<T>::get(std::size_t index) const {
+    if (index >= size || !head) {
+        // Handle error, such as returning a default value or throwing an exception
+        return T();
+    }
+    Node* current = head;
+    for (std::size_t i = 0; i < index; ++i) {
+        current = current->next;
+    }
+    return current->data;
+}
+
+template <typename T>
+void List<T>::set(std::size_t index, const T& value) {
+    if (index >= size || !head) {
+        return;  // Invalid index or empty list
+    }
+    Node* current = head;
+    for (std::size_t i = 0; i < index; ++i) {
+        current = current->next;
+    }
+    current->data = value;
+}
+
+template <typename T>
+std::size_t List<T>::getSize() const {
+    return size;
+}
+
+#endif // LIST_H
--- core/templates/vector.h
+++ core/templates/vector.h
@@ -1,153 +1,189 @@
-/**
- * MIT License
-
-Copyright (c) 2024/2025 rPatsher
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <iostream>
+#include <algorithm>
 
-#include <array>
-#include <vector>
-
-#include <cstdint> 
-
-#include "core/templates/map.h"
-#include "core/variant/dictionary.h"
-
-template <typename T>
+/**
+ * @brief A simple dynamic array implementation similar to std::vector.
+ * @tparam T The type of elements stored in the vector.
+ */
+template<typename T>
 class Vector {
 private:
-    T* array;
-    size_t capacity;
-    size_t size;
+    T* data;        ///< Pointer to the dynamic array.
+    size_t size;     ///< Current number of elements in the vector.
+    size_t capacity; ///< Current capacity of the vector.
 
 public:
-    // Constructor
-    Vector() : array(nullptr), capacity(0), size(0) {}
+    /**
+     * @brief Default constructor for the Vector class.
+     */
+    Vector() : data(nullptr), size(0), capacity(0) {}
 
-    // Destructor
+    /**
+     * @brief Destructor for the Vector class.
+     */
     ~Vector() {
-        delete[] array;
+        delete[] data;
     }
 
-    void push_back(const T& element) {
-        if (size >= capacity) {
-            size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
-            T* new_array = new T[new_capacity];
-            for (size_t i = 0; i < size; ++i) {
-                new_array[i] = array[i];
-            }
-            delete[] array;
-            array = new_array;
-            capacity = new_capacity;
-        }
-        array[size++] = element;
-    }
-    
-    
+    /**
+     * @brief Get the current size of the vector.
+     * @return The number of elements in the vector.
+     */
     size_t getSize() const {
         return size;
     }
 
-   void fill(const T& p_elem) {
-    for (size_t i = 0; i < size; ++i) {
-        array[i] = p_elem;
+    /**
+     * @brief Get the current capacity of the vector.
+     * @return The maximum number of elements the vector can hold without reallocation.
+     */
+    size_t getCapacity() const {
+        return capacity;
     }
 
-    }
-    void remove_at(int p_index) {
-    if (p_index < 0 || p_index >= size) {
-        throw std::out_of_range("Index out of range");
-    }
-    for (size_t i = p_index; i < size - 1; ++i) {
-        array[i] = array[i + 1];
-    }
-    size--;
+    /**
+     * @brief Add an element to the end of the vector.
+     * @param value The value to be added to the vector.
+     */
+    void push_back(const T& value) {
+        if (size == capacity) {
+            if (capacity == 0) {
+                capacity = 1;
+            } else {
+                capacity *= 2;
+            }
+
+            T* newData = new T[capacity];
+            std::copy(data, data + size, newData);
+            delete[] data;
+            data = newData;
+        }
+
+        data[size++] = value;
     }
 
-    Vector<T> slice(int p_begin, int p_end = INT_MAX) const {
-    p_end = (p_end > size) ? size : p_end;
-    p_begin = (p_begin < 0) ? 0 : p_begin;
+    /**
+     * @brief Insert an element at a specific position in the vector.
+     * @param pos Iterator pointing to the position where the element will be inserted.
+     * @param value The value to be inserted.
+     * @return Iterator pointing to the newly inserted element.
+     */
+    typename T* insert(typename T* pos, const T& value) {
+        size_t index = pos - data;
+        if (index > size) {
+            throw std::out_of_range("Insert position out of range");
+        }
 
-    if (p_begin >= p_end) {
-        return Vector<T>();
+        if (size == capacity) {
+            if (capacity == 0) {
+                capacity = 1;
+            } else {
+                capacity *= 2;
+            }
+
+            T* newData = new T[capacity];
+            std::copy(data, data + index, newData);
+            newData[index] = value;
+            std::copy(data + index, data + size, newData + index + 1);
+            delete[] data;
+            data = newData;
+            size++;
+        } else {
+            std::copy_backward(data + index, data + size, data + size + 1);
+            data[index] = value;
+            size++;
+        }
+
+        return data + index;
     }
 
+    /**
+     * @brief Erase an element at a specific position in the vector.
+     * @param pos Iterator pointing to the element to be erased.
+     * @return Iterator pointing to the element after the erased element.
+     */
+    typename T* erase(typename T* pos) {
+        size_t index = pos - data;
+        if (index >= size) {
+            throw std::out_of_range("Erase position out of range");
+        }
 
+        std::copy(data + index + 1, data + size, data + index);
+        size--;
 
-    Vector<T> result;
-    for (int i = p_begin; i < p_end; ++i) {
-        result.push_back(array[i]);
+        return data + index;
     }
-    return result;
-}
-template <class T>
-bool operator!=(const Vector<T>& p_arr) const {
-    if (size != p_arr.size) {
-        return true; 
+
+    /**
+     * @brief Sort the elements in the vector.
+     */
+    void sort() {
+        std::sort(data, data + size);
     }
 
-    for (size_t i = 0; i < size; ++i) {
-        if (array[i] != p_arr[i]) {
-            return true;
+    /**
+     * @brief Remove all elements from the vector.
+     */
+    void clear() {
+        delete[] data;
+        data = nullptr;
+        size = 0;
+        capacity = 0;
+    }
+
+    /**
+     * @brief Remove the last element from the vector.
+     */
+    void popBack() {
+        if (size > 0) {
+            size--;
         }
     }
 
-    return false;  
-}
-
-Vector<uint8_t> to_byte_array() const {
-    Vector<uint8_t> byte_array;
-    for (size_t i = 0; i < size; ++i) {
-        uint8_t* bytes = reinterpret_cast<uint8_t*>(&array[i]);
-        for (size_t j = 0; j < sizeof(T); ++j) {
-            byte_array.push_back(bytes[j]);
+    /**
+     * @brief Change the size of the vector.
+     * @param newSize The new size of the vector.
+     * @param value The value to initialize new elements (if any).
+     */
+    void resize(size_t newSize, const T& value = T()) {
+        if (newSize > size) {
+            if (newSize > capacity) {
+                reserve(newSize);
+            }
+            std::fill(data + size, data + newSize, value);
+        } else if (newSize < size) {
+            size = newSize;
         }
     }
-    return byte_array;
-}
 
+    /**
+     * @brief Reserve a new capacity for the vector.
+     * @param newCapacity The new capacity to reserve.
+     */
+    void reserve(size_t newCapacity) {
+        if (newCapacity > capacity) {
+            T* newData = new T[newCapacity];
+            std::copy(data, data + size, newData);
+            delete[] data;
+            data = newData;
+            capacity = newCapacity;
+        }
+    }
 
-
-    // Function to access elements by index
+    /**
+     * @brief Access an element in the vector by index.
+     * @param index The index of the element to access.
+     * @return A reference to the element at the specified index.
+     * @throws std::out_of_range if the index is out of bounds.
+     */
     T& operator[](size_t index) {
         if (index >= size) {
             throw std::out_of_range("Index out of range");
         }
-        return array[index];
-    }
-
-     T& operator[](size_t index) {
-        if (index >= size) {
-            throw std::out_of_range("???? ???? ??????");
-        }
-        return array[index];
-    }
-
-    // ??? ???????? ????? ?????
-    void fill(const T& p_elem) {
-        for (size_t i = 0; i < size; ++i) {
-            array[i] = p_elem;
-        }
+        return data[index];
     }
 };
 
